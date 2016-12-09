ef78a8c1133bb802b37c07561272c740c1f67d1e
ryantantiern <ryantantiern@gmail.com>
2016-12-09 23:45:26 +0000

    first commit from ryan, fb auth for dev

---
+++ webserver/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<script src="dev_auth.js"></script>
+	hello world!
+
+	<div
+	  class="fb-like"
+	  data-share="true"
+	  data-width="450"
+	  data-show-faces="true">
+	</div>
+</body>
+</html>
