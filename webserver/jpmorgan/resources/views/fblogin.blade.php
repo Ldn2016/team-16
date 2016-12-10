c5289489908af79aaae9dbe73d821fc6f1a2ce0c
ryantantiern <ryantantiern@gmail.com>
2016-12-10 08:05:39 +0000

    latest ryan

--- webserver/jpmorgan/resources/views/fblogin.blade.php
+++ webserver/jpmorgan/resources/views/fblogin.blade.php
@@ -1,24 +1,11 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>jpstrangers</title>
-	<script src="js/fb.js"></script>
-Try it Yourself »
-</head>
-<body>
-	<div id="fb-root"></div>
+@extends('welcome')
 
+@section('content')
+	<div class="row">
+	      <div class="col-md-6 col-md-offset-3">
+	      	<h2>Login Using Facebook</h2>
+	          <h2><a class="btn btn-primary" href="{{ url('auth/facebook') }}">Facebook</a></h2>
+	      </div>
+	  </div>
 
-	<div class="fb-login-button" data-max-rows="1" data-size="large" data-show-faces="true" data-auto-logout-link="true" scope="user_photos,user_relationships,user_relationship_details">
-		
-	</div>
-		<button id="button" onclick="getMeYou()">get photos</button>
-	<div id="imgs">
-		
-	</div>
-
-
-
-
-</body>
-</html>
+@endsection
