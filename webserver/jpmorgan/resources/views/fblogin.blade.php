a55729e120798533b8a1e2b6693a32e1e0c449a4
ryantantiern <ryantantiern@gmail.com>
2016-12-10 05:10:39 +0000

    made changes

--- webserver/jpmorgan/resources/views/fblogin.blade.php
+++ webserver/jpmorgan/resources/views/fblogin.blade.php
@@ -2,14 +2,14 @@
 <html>
 <head>
 	<title>jpstrangers</title>
-	<script src="index.js"></script>
+	<script src="js/fb.js"></script>
 Try it Yourself »
 </head>
 <body>
 	<div id="fb-root"></div>
 
 
-	<div class="fb-login-button" data-max-rows="1" data-size="large" data-show-faces="false" data-auto-logout-link="false" scope="user_photos">
+	<div class="fb-login-button" data-max-rows="1" data-size="large" data-show-faces="true" data-auto-logout-link="true" scope="user_photos,user_relationships,user_relationship_details">
 		
 	</div>
 		<button id="button" onclick="getMeYou()">get photos</button>
